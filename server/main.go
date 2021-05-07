69a6409dd6ff1f73c29aefef656cdebbb102473d
Lyubosumaz <lyuboslavilkov@gmail.com>
2021-05-08 01:08:05 +0300

    update go server

--- server/main.go
+++ server/main.go
@@ -1,10 +1,26 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"unicode/utf8"
+)
+
+var port string = ":8080"
+
+func trimFirstRune(s string) string {
+	_, i := utf8.DecodeRuneInString(s)
+	return s[i:]
+}
 
 func main() {
 	http.HandleFunc("/hello-world", func (w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("hello world"))
 	})
-	http.ListenAndServe(":8080", nil)
+
+	fmt.Printf("Starting server at port %s\n", trimFirstRune(port))
+ 	if err := http.ListenAndServe(port, nil); err != nil {
+		 log.Fatal(err)
+	 }
 }
