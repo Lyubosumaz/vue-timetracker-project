34c9b6b746d9e7eb14e58362f6d73df243d14a58
Lyubosumaz <LyuboslavIlkov@gmail.com>
2021-05-19 13:10:06 +0300

    adding in lists

--- server_py/lists.py
+++ server_py/lists.py
@@ -21,4 +21,39 @@ first_col = [row[0] for row in matrix]
 print(first_col)                            # [1, 4, 7]
 
 # DICTIONARIES || HASH TABLES || OBJECT IN JS
-my_stuff = {"key1": 'value', 'key2': 'value'}
+my_stuff = {"key1": 'value1', 'key2': 'value2', 'key3': {'123': [1, 2, 3, 'grabMe']}}
+print(my_stuff['key1'])
+print(my_stuff['key2'])
+print(my_stuff['key3']['123'][3].upper())
+
+my_food = {'lunch': "pizza", "breakfast": 'eggs'}
+my_food['lunch'] = 'burger'
+my_food['dinner'] = 'pasta'
+print(my_food['lunch'])
+print(my_food)
+
+# TUPLES, SET, BOOLEANS
+True
+False
+
+my_tuple = (1, 2, 3)
+print(my_tuple[0])
+
+my_tuple = ('a', True, 123)
+print(my_tuple)
+# my_tuple[0] = 'NEW'  # throw an error, tuple is immutable like string is
+
+my_set = set()
+my_set.add(1)
+my_set.add(5)
+my_set.add(8)
+my_set.add(8)
+my_set.add(8)
+my_set.add(8)
+my_set.add(8)
+my_set.add(-2)
+my_set.add(0.3)
+print(my_set)
+
+x = set([1, 1, 1, 1, 1, 1, 1, 3, 3, 4, 4, 3, 3, 3, 4, 3])
+print(x)
